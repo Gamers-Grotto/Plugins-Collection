a813bba36a7ae923e332b5c1b8fd02f882fd154a
Michael Van Der Poll <michaelvdp@gmail.com>
2024-10-05 13:50:37 +0200

    adds starting point for using scriptable object audio events

---
+++ Assets/Plugins/GamersGrotto/Runtime/Modules/ScriptableAudioSystem/AudioEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GamersGrotto.Runtime.Modules.ScriptableAudioSystem
+{
+    [CreateAssetMenu(fileName = "New Audio Event", menuName = "GamersGrotto/Scriptable Audio System/Audio Event")]
+    public class AudioEvent : ScriptableObject
+    {
+        public AudioClip clip;
+        public float volume = 1.0f;
+        public bool loop = false;
+
+        public void Play(AudioSource audioSource)
+        {
+            if (clip == null || audioSource == null) 
+                return;
+        
+            audioSource.clip = clip;
+            audioSource.volume = volume;
+            audioSource.loop = loop;
+            audioSource.Play();
+        }
+    }
+}
