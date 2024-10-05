d8f1d86294223eb0f2d89db6e4c962c3cf21c3e4
Michael Van Der Poll <michaelvdp@gmail.com>
2024-10-05 10:10:39 +0200

    adds easy access to collision and trigger events in inspector

--- Assets/Plugins/GamersGrotto/Runtime/Core/Extensions.cs
+++ Assets/Plugins/GamersGrotto/Runtime/Core/Extensions.cs
@@ -4,7 +4,11 @@ namespace GamersGrotto.Runtime.Core
 {
     public static class Extensions
     {
+        //Vector Extensions
         public static Vector2 To(this Vector2 source, Vector2 target) => target - source;
         public static Vector3 To(this Vector3 source, Vector3 target) => target - source;
+        
+        //GameObject Extensions
+        public static bool IsInLayerMask(this GameObject obj, LayerMask mask) => (mask.value & (1 << obj.layer)) != 0;
     }
 }
---
+++ Assets/Plugins/GamersGrotto/Runtime/Modules/CollisionTriggers/CollisionEvents2D.cs
@@ -0,0 +1,39 @@
+﻿using GamersGrotto.Runtime.Core;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GamersGrotto.Runtime.Modules.CollisionTriggers
+{
+    public class CollisionEvents2D : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerMask;
+        
+        public UnityEvent<GameObject> onEnterCollisionEvent;
+        public UnityEvent<GameObject> onStayCollisionEvent;
+        public UnityEvent<GameObject> onExitCollisionEvent;
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if(!other.gameObject.IsInLayerMask(layerMask))
+                return;
+            
+            onEnterCollisionEvent?.Invoke(other.gameObject);
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            if(!other.gameObject.IsInLayerMask(layerMask))
+                return;
+            
+            onStayCollisionEvent?.Invoke(other.gameObject);
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if(!other.gameObject.IsInLayerMask(layerMask))
+                return;
+            
+            onExitCollisionEvent?.Invoke(other.gameObject);
+        }
+    }
+}
