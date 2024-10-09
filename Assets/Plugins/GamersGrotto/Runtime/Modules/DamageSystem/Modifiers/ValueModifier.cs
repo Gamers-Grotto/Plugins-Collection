35a43eaffa18792057038671939918bea99012ba
Oliver Källerfors <idetorka@hotmail.com>
2024-10-09 16:25:44 +0200

    Removed: [Min(0)] from ValueModifier modifier field

--- Assets/Plugins/GamersGrotto/Runtime/Modules/DamageSystem/Modifiers/ValueModifier.cs
+++ Assets/Plugins/GamersGrotto/Runtime/Modules/DamageSystem/Modifiers/ValueModifier.cs
@@ -5,7 +5,7 @@ namespace GamersGrotto.Runtime.Modules.DamageSystem {
     /// Separated to its own abstract class to allow for easier sorting between modifiers.
     /// </summary>
     public abstract class ValueModifier : ScriptableObject {
-        [Min(0)] public float modifier = 1f;
+        public float modifier = 1f;
 
         /// <summary>
         /// Returns the amount modified by the modifier.
