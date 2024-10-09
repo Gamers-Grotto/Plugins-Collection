ec261a53dc1b82c3749e1911a978f86abc85f85f
Oliver Källerfors <idetorka@hotmail.com>
2024-10-09 17:13:29 +0200

    Added: AdditiveDamageModifier

---
+++ Assets/Plugins/GamersGrotto/Runtime/Modules/DamageSystem/Modifiers/AdditiveDamageModifier.cs
@@ -0,0 +1,10 @@
+﻿namespace GamersGrotto.Runtime.Modules.DamageSystem {
+    public class AdditiveDamageModifier: ValueModifier {
+        /// <summary>
+        /// Returns the amount added by the modifier.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public override float ApplyModifier(float amount) => amount + modifier;
+    }
+}
