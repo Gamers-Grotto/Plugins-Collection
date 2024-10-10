015780ac7fd0b06e02a51c4e6a1436c7998ebc43
Oliver Källerfors <idetorka@hotmail.com>
2024-10-10 18:36:26 +0200

    Changed: Max volume from 2 to 1.
    Reason: Audio Support doesn't support values other than 0-1

--- Assets/Plugins/GamersGrotto/Runtime/Modules/AudioSystem/AudioEvents/AudioEvent.cs
+++ Assets/Plugins/GamersGrotto/Runtime/Modules/AudioSystem/AudioEvents/AudioEvent.cs
@@ -6,7 +6,7 @@ namespace GamersGrotto.Runtime.Modules.GameEvents.AudioEvents
     public class AudioEvent : ScriptableObject
     {
         public AudioClip clip;
-        [Range(0,2)]public float volume = 1.0f;
+        [Range(0,1)]public float volume = 1.0f;
         [Range(-3,3)]public float pitch = 1.0f;
 
         public void Play(AudioSource audioSource, bool loop)
