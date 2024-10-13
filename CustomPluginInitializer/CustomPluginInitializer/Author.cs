92d0b9de6ca738a8be42c731c72730a15a6912ea
Oliver Källerfors <idetorka@hotmail.com>
2024-10-13 14:50:48 +0200

    Removed: Depricated CustomPluginIntializer
    New version already added within the unity project

--- CustomPluginInitializer/CustomPluginInitializer/Author.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomPluginInitializer;
-
-public class Author
-{
-    public string name { get; set; }
-    public string email { get; set; }
-    public string url { get; set; }
-}
--- CustomPluginInitializer/CustomPluginInitializer/Interfaces/IFile.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace CustomPluginInitializer.Interfaces;
-
-public interface IFile
-{
-    [JsonIgnore] public string FileName { get; set; } // Put Attribute on every instance.
-    public string GenerateFileContent();
-}
