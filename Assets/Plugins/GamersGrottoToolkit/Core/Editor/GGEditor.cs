71d1582c19d976c1050dd29ccc1a4d972c1df90e
Michael van der Poll <michaelvdp@gmail.com>
2024-10-19 23:26:54 +0200

    adds custom editor branding for GamersGrotto monoB

---
+++ Assets/Plugins/GamersGrottoToolkit/Core/Editor/GGEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GamersGrotto.Core.Editor
+{
+    [CustomEditor(typeof(MonoBehaviour), editorForChildClasses: true)]
+    public class GGEditor : UnityEditor.Editor
+    {
+        private static Texture2D logo;
+
+        private bool isInGamersGrottoNamespace;
+        
+        private void OnEnable()
+        {
+            var targetNamespace = target.GetType().Namespace;
+            isInGamersGrottoNamespace = targetNamespace?.StartsWith("GamersGrotto") ?? false;
+            
+            if(logo == null)
+                logo = Resources.Load<Texture2D>("GGLogo");
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!isInGamersGrottoNamespace) 
+                return;
+            
+            using (new GUILayout.HorizontalScope())
+            {
+                if (logo != null)
+                    GUILayout.Label(logo, GUILayout.Width(64), GUILayout.Height(64));
+                    
+                using (new GUILayout.VerticalScope())
+                {
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label("Gamers Grotto");
+                    GUILayout.FlexibleSpace();
+                }
+            }
+        }
+    }
+}
