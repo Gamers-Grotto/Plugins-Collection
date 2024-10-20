643939a1ee2fc6780f2a67fd10ecca0b5845b5d8
Oliver Källerfors <idetorka@hotmail.com>
2024-10-20 23:57:23 +0200

    Refactored: GGEditor

--- Assets/Plugins/GamersGrottoToolkit/Core/Editor/GGEditor.cs
+++ Assets/Plugins/GamersGrottoToolkit/Core/Editor/GGEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using GamersGrotto.Core.Extended_Attributes;
 using GamersGrotto.Custom_Editor_Styles.Editor;
@@ -11,47 +12,54 @@ namespace GamersGrotto.Core.Editor
     public class GGEditor : UnityEditor.Editor
     {
         private bool isInGamersGrottoNamespace;
-        
+
         private void OnEnable()
         {
             var targetNamespace = target.GetType().Namespace;
             isInGamersGrottoNamespace = targetNamespace?.StartsWith("GamersGrotto") ?? false;
         }
-        
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+            DrawButtons();
 
+            if (isInGamersGrottoNamespace)
+            {
+                DrawLogo();
+            }
+        }
+
+        private void DrawButtons()
+        {
             var mono = target as MonoBehaviour;
-            var type = target.GetType();
-            
-            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            var methods = target.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
 
-            foreach (var method in methods)
+            foreach (var method in methods.Where(m => Attribute.IsDefined(m, typeof(ButtonAttribute))))
             {
                 var buttonAttribute = (ButtonAttribute)Attribute.GetCustomAttribute(method, typeof(ButtonAttribute));
-                
-                if (buttonAttribute == null) 
-                    continue;
-
-                var buttonText = buttonAttribute.ButtonText ?? method.Name;
+                var buttonText = buttonAttribute?.ButtonText ?? method.Name;
 
                 if (GUILayout.Button(buttonText))
+                {
                     method.Invoke(mono, null);
+                }
             }
-            
-            if (!isInGamersGrottoNamespace) 
-                return;
-            
+        }
+
+        private void DrawLogo()
+        {
             using (new GUILayout.HorizontalScope())
             {
                 if (CustomEditorStyles.Logo != null)
+                {
                     GUILayout.Label(CustomEditorStyles.Logo, GUILayout.Width(64), GUILayout.Height(64));
-                    
+                }
+
                 using (new GUILayout.VerticalScope())
                 {
                     GUILayout.FlexibleSpace();
-                    GUILayout.Label("Gamers Grotto",CustomEditorStyles.SubHeaderLabel);
+                    GUILayout.Label("Gamers Grotto", CustomEditorStyles.SubHeaderLabel);
                     GUILayout.FlexibleSpace();
                 }
             }
