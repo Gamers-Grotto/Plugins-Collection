680721e007be846705b673e9e439225c0af4ce69
Michael van der Poll <michaelvdp@gmail.com>
2024-10-26 14:28:50 +0200

    Adds RangedFloat type, and property drawer for it

---
+++ Assets/Plugins/GamersGrottoToolkit/Core/Runtime/Extended Attributes/MinMaxRangedAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GamersGrotto.Core.Extended_Attributes
+{
+    public class MinMaxRangeAttribute : Attribute
+    {
+        public float Min { get; private set; }
+        
+        public float Max { get; private set; }
+        
+        public MinMaxRangeAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
