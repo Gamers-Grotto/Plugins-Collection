b47e4638344a67dbfdc2a00a4cd3dec439a10971
Michael van der Poll <michaelvdp@gmail.com>
2024-11-02 11:35:43 +0100

    adds boucepad and prefabs

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GamersGrotto.Core;
+using UnityEngine;
+
+namespace GamersGrotto
+{
+    public class BouncePad : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField] private float bounceForce;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(!other.gameObject.IsInLayerMask(layerMask)) 
+                return;
+
+            if (other.gameObject.TryGetComponent<Rigidbody>(out var rb))
+            {
+                rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
