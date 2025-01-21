c2ac685119b66fbd01a33ccdd973f0113d9196a0
Oliver Källerfors <idetorka@hotmail.com>
2025-01-21 14:39:06 +0100

    Added CollisionDamageDealer
    Implemented in Sample:
    - 3x Spike variants
    - Spike Damage Rework

--- Assets/Plugins/GamersGrottoToolkit/Modules/Damage System/Runtime/CollisionDamageDealer.cs
+++ Assets/Plugins/GamersGrottoToolkit/Modules/Damage System/Runtime/CollisionDamageDealer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using GamersGrotto.Core;
 using GamersGrotto.Damage_System;
 using UnityEngine;
@@ -5,20 +6,37 @@ using UnityEngine;
 public class CollisionDamageDealer : MonoBehaviour
 {
     [SerializeField] private DamageComponent damageComponent;
-    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] protected List<LayerMask> enemyLayers;
     void Awake() {
        damageComponent = GetComponent<DamageComponent>();
     }
 
     void OnCollisionEnter(Collision other) {
-        if (!other.gameObject.IsInLayerMask(enemyLayer))
-            return;
+        var enemyInLayer = false;
+        foreach (var enemyLayer in enemyLayers) {
+            if (other.gameObject.IsInLayerMask(enemyLayer)) {
+                enemyInLayer = true;
+                break;
+            }
+        }
         
+        if (!enemyInLayer)
+            return;
         var health = other.gameObject.GetComponent<Health>();
         if (health == null) {
             Debug.LogWarning($"[{gameObject.name}] Tried to deal damage to [{other.gameObject.name}] but it does not have a Health component.", this);
             return;
         }
         damageComponent.DealDamage(health);
+
+        DoChildLogic(other);
+    }
+    
+    /// <summary>
+    /// Is called after the damage has been dealt to the object.
+    /// </summary>
+    /// <param name="other"></param>
+    public virtual void DoChildLogic(Collision other) {
+        //This is a virtual method that can be overridden by child classes for added functionality
     }
 }
