8e926cf8db575ffbd032e55c1488a1da22815ec9
Oliver Källerfors <idetorka@hotmail.com>
2025-03-13 15:25:22 +0100

    Created Initial Multiplayer Sample Setup

---
+++ Assets/Samples/Multiplayer Sample/Scripts/TitleScreenManager.cs
@@ -0,0 +1,12 @@
+using GamersGrotto.Core.Extended_Attributes;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GamersGrotto.MultiplayerSample {
+    public class TitleScreenManager : MonoBehaviour {
+        [Button]
+        public void StartNetworkAsHost() {
+            NetworkManager.Singleton.StartHost();
+        }
+    }
+}
