78e29b5320b08e8f0b82412214c4501f382cb7a3
Michael van der Poll <michaelvdp@gmail.com>
2025-03-15 13:04:41 +0100

    adds health and health ui to player

---
+++ Assets/Samples/Multiplayer Sample/Scripts/NetworkHealth.cs
@@ -0,0 +1,37 @@
+﻿using GamersGrotto.Core.Extended_Attributes;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GamersGrotto.Multiplayer_Sample
+{
+    public class NetworkHealth : NetworkBehaviour
+    {
+        [SerializeField] private NetworkVariable<float> health;
+        [SerializeField] private float maxHealth;
+        
+        public UnityEvent<float> onHealthChanged = new ();
+        
+        public void TakeDamage(float damage)
+        {
+            health.Value -= damage;
+            onHealthChanged.Invoke(health.Value / maxHealth);
+        }
+
+        public void Heal(float heal)
+        {
+            health.Value += heal;
+            onHealthChanged.Invoke(health.Value / maxHealth);
+        }
+        
+        #region Testing
+
+        [Button]
+        public void TestDamage()
+        {
+            TakeDamage(Random.Range(1, 15));
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Samples/Multiplayer Sample/Scripts/PlayerWorldSpaceUI.cs
@@ -0,0 +1,46 @@
+﻿using GamersGrotto.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GamersGrotto.Multiplayer_Sample
+{
+    public class PlayerWorldSpaceUI : MonoBehaviour
+    {
+        [SerializeField] private Image healthFill;
+        [SerializeField] private TMP_Text playerNameText;
+        [SerializeField] private NetworkHealth networkHealth;
+        [SerializeField] private Camera playerCamera;
+
+        private void OnEnable()
+        {
+            networkHealth.onHealthChanged.AddListener(OnHealthChanged);
+        }
+
+        private void OnDisable()
+        {
+            networkHealth.onHealthChanged.RemoveListener(OnHealthChanged);
+        }
+
+        private void OnHealthChanged(float healthNormalized)
+        {
+            SetHealth(healthNormalized);
+        }
+
+        public void SetHealth(float healthNormalized)
+        {
+            healthFill.fillAmount = healthNormalized;
+        }
+
+        public void SetPlayerName(string playerName)
+        {
+            playerNameText.text = playerName;
+        }
+
+        private void Update()
+        {
+            var desired = playerCamera.transform.position.To(transform.position);
+            transform.rotation = Quaternion.LookRotation(desired);
+        }
+    }
+}
