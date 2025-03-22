b32d6382bc39341e5cbd99e7ae1eb3bf2c33989b
Oliver Källerfors <idetorka@hotmail.com>
2025-03-22 19:21:05 +0100

    Implemented: Player Name Display

---
+++ Assets/Samples/Multiplayer Sample/Scripts/PlayeDataController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GamersGrotto.Multiplayer_Sample
+{
+    public class PlayeDataController : MonoBehaviour
+    {
+        [SerializeField] private PlayerWorldSpaceUI playerWorldSpaceUI;
+        
+        private void Start()
+        {
+           SessionManager.Instance.OnPlayerDataChangedEvent.AddListener(OnClientConnected);
+        }
+
+        void OnDisable() {
+            SessionManager.Instance.OnPlayerDataChangedEvent.RemoveListener(OnClientConnected);
+        }
+
+        void OnClientConnected(List<PlayerSessionData> _) {
+            var ownerID = GetComponent<NetworkObject>().OwnerClientId;
+            playerWorldSpaceUI.SetPlayerName(SessionManager.Instance.GetPlayerName(ownerID));
+        }
+    }
+}
--- Assets/Samples/Multiplayer Sample/Scripts/PlayerWorldSpaceUI.cs
+++ Assets/Samples/Multiplayer Sample/Scripts/PlayerWorldSpaceUI.cs
@@ -4,61 +4,41 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace GamersGrotto.Multiplayer_Sample
-{
-    public class PlayerWorldSpaceUI : MonoBehaviour
-    {
+namespace GamersGrotto.Multiplayer_Sample {
+    public class PlayerWorldSpaceUI : MonoBehaviour {
         [SerializeField] private Image healthFill;
         [SerializeField] private TMP_Text playerNameText;
         [SerializeField] private NetworkHealth networkHealth;
-        
+
         private Camera mainCam;
-        
-        private void OnEnable()
-        {
+
+        private void OnEnable() {
             networkHealth.onHealthChanged.AddListener(OnHealthChanged);
             mainCam = Camera.main;
-            
-            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
         }
 
-        
-
-        private void OnDisable()
-        {
+        private void OnDisable() {
             networkHealth.onHealthChanged.RemoveListener(OnHealthChanged);
-            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
-        }
-        
-        void OnClientConnected(ulong connectingClientId) {
-            Debug.Log($"Client connected - PlayerworldSpaceUI {connectingClientId}");
-            //Set other players names
-            //Set own name
-            
         }
 
-        private void OnHealthChanged(float healthNormalized)
-        {
+
+        private void OnHealthChanged(float healthNormalized) {
             SetHealth(healthNormalized);
         }
 
-        public void SetHealth(float healthNormalized)
-        {
+        public void SetHealth(float healthNormalized) {
             healthFill.fillAmount = healthNormalized;
         }
 
-        public void SetPlayerName(string playerName)
-        {
+        public void SetPlayerName(string playerName) {
             playerNameText.text = playerName;
         }
 
-        private void Update()
-        {
+        private void Update() {
             if (mainCam == null)
                 mainCam = Camera.main;
 
-            if (mainCam != null)
-            {
+            if (mainCam != null) {
                 var desired = mainCam.transform.position.To(transform.position);
                 transform.rotation = Quaternion.LookRotation(desired);
             }
