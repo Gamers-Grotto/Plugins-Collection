a4edfc8adf4a7b7ab63b59b962d0ca1e0e19e873
Michael van der Poll <michaelvdp@gmail.com>
2025-03-22 13:58:12 +0100

    adds fireball ability for player

---
+++ Assets/Samples/Multiplayer Sample/Scripts/CombatController.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace GamersGrotto.Multiplayer_Sample
+{
+    public class CombatController : NetworkBehaviour
+    {
+        public GameObject fireballPrefab;
+        public Transform fireballSpawnPoint;
+        public float fireballSpeed = 10f;
+
+        private void Update()
+        {
+            if (!IsOwner)
+                return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                RequestFireballServerRpc();
+            }
+        }
+        
+        [ServerRpc]
+        private void RequestFireballServerRpc()
+        {
+            var fireballInstance = Instantiate(fireballPrefab, fireballSpawnPoint.position, fireballSpawnPoint.rotation);
+            var networkObject = fireballInstance.GetComponent<NetworkObject>();
+
+            if (networkObject != null)
+            {
+                networkObject.Spawn(true);
+                fireballInstance.GetComponent<Rigidbody>().linearVelocity = fireballSpawnPoint.forward * fireballSpeed;
+            }
+        }
+    }
+}
