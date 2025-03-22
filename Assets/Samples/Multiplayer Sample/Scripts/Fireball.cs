b0396df2937532279cc0f27c3a32eea2462571b0
Michael van der Poll <michaelvdp@gmail.com>
2025-03-22 14:25:09 +0100

    changes firepoint for fireball and adds collision

---
+++ Assets/Samples/Multiplayer Sample/Scripts/Fireball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GamersGrotto.Multiplayer_Sample
+{
+    public class Fireball : NetworkBehaviour
+    {
+        public float damage = 10f;
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if(!IsServer)
+                return;
+            
+            if (other.gameObject.TryGetComponent<NetworkHealth>(out var health))
+            {
+                health.TakeDamageServerRpc(damage);
+            }
+            
+            GetComponent<NetworkObject>().Despawn(true);
+        }
+    }
+}
